1c9d14caa4a6b46749a2c4b59fcb49eabac489b0
Udeme98 <darealudeme1@gmail.com>
2024-01-13 06:25:32 +0100

    modified script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//The Console
 console.log("Hello World");
 console.warn("This is a warning");
 console.error("This is an error warning");
+
+//Adding styles to your console
+const style = "padding:12px; background-Color:green; color:white;";
+
+console.log("%cUdeme", style);
