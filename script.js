2086d6fad0537819a33d8edafeb8072cb3f97a15
Udeme98 <Darealudeme1@gmail.com>
2024-01-14 10:08:54 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@
 // console.log("%cUdeme", style);
 
 // Comments - Comments are used for documenting your code, explaing what a code does and also to disable codes
+
+//Variables - Containers for pieces of data (var, let & const)
+// let age = 25;
+// console.log(age);
+
+// let arr = [1, 2, 3, 4];
+// arr.push(5);
+// console.log(arr);
+
+//Data Types (Primitive)
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt;
+// Data Types (Reference Types)
+// -Function
+// -Object literals
+// -Arrays
+
+const names = "Udeme";
+console.log(names + " datatype is a " + typeof names);
