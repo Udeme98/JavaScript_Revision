78684e4a72a271c56084c840542aa116954d9bda
Udeme98 <Darealudeme1@gmail.com>
2024-01-14 06:34:29 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //The Console
-console.log("Hello World");
-console.warn("This is a warning");
-console.error("This is an error warning");
+// console.log("Hello World");
+// console.warn("This is a warning");
+// console.error("This is an error warning");
 
 //Adding styles to your console
-const style = "padding:12px; background-Color:green; color:white;";
+// const style = "padding:12px; background-Color:green; color:white;";
+// console.log("%cUdeme", style);
 
-console.log("%cUdeme", style);
+// Comments - Comments are used for documenting your code, explaing what a code does and also to disable codes
