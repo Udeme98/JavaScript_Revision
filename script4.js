f3b86db01c2e56912791a1b68b7da6ad4f3b2cd5
Udeme98 <Darealudeme1@gmail.com>
2024-01-18 13:49:28 +0100

    updated script4.js

--- script4.js
+++ script4.js
@@ -146,3 +146,78 @@ if (Object.keys(user).length > 0) {
 } else {
   // console.log("No user");
 }
+
+//loose equality
+//It's most prefarable to use double equals in your code
+
+//Logical Operator
+
+//Logical AND Operator
+// console.log(10 > 6 && 8 > 10);
+
+//Logical OR Operator
+// console.log(10 > 6 || 8 > 10);
+
+// &&- returns a first falsy value or the last value
+let a;
+a = 10 && 20;
+a = "" && 0 && 20;
+// console.log(a);
+
+const arr1 = ["post"];
+// arr1.length > 0 && console.log(arr1[0]);
+
+// ||- returns a first truthy value or the last value
+let b;
+b = 10 || 20;
+b = 0 || 20;
+// console.log(b);
+
+// ?? - returns the right side operand when the left is 'null' or 'undefined' only
+let c;
+c = 10 ?? 20;
+c = null ?? 20;
+// console.log(c);
+
+//Logical Assignment
+// ||= - assigns the right side value only if the left is a falsy value
+let e = false;
+// if (!e) {
+//   e = 20;
+// }
+
+// e = e || 20;
+e ||= 20; // using logical OR assignment
+// console.log(e);
+
+// &&= - assigns the right side value only if the left is a truthy value
+// ??= - assigns the right side value only if the left is a 'null' or 'undefined' value
+
+//Ternary Operator
+
+const age = 20;
+//using normal if statement
+// if (age >= 18) {
+//   // console.log("You can vote!");
+// } else {
+//   console.log("You cannot vote!");
+// }
+
+//using ternary operator
+age >= 18 ? console.log("You can vote!") : console.log("You cannot vote yet");
+
+//Assigning a conditional value to a variable
+const canVote = age >= 18 ? true : false;
+console.log(canVote);
+
+// Multilple statement
+const auth = true;
+
+// let redirect = auth
+//   ? (alert("Welcome"), "/dashboard")
+//   : (alert("Please Login"), "/login");
+
+// console.log(redirect);
+
+// shorthand using &&
+auth && console.log("Welcome to the dashboard");
