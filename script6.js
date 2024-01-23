560fd04643ff275202b4a2b1eb0955f12666c395
Udeme98 <Darealudeme1@gmail.com>
2024-01-23 11:38:49 +0100

    updated script6.js

--- script6.js
+++ script6.js
@@ -1,6 +1,20 @@
 // Higher Order Array Methods;
+
 // forEach
 const socials = ["Twitter", "LinkedIn", "Instagram"];
-socials.forEach((social) => {
+// socials.forEach((social) => console.log(social));
+// socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+
+//using named functions
+function listSocials(social) {
   console.log(social);
-});
+}
+socials.forEach(listSocials);
+
+//using array of objects
+const socialObjs = [
+  { name: "James", social: "Twitter" },
+  { name: "John", social: "Facebook" },
+  { name: "Joe", social: "LinkedIn" },
+  { name: "Jane", social: "Instagram" },
+];
