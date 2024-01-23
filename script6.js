a90057143c0f8720d83892ef1cc192192504d4fd
Udeme98 <Darealudeme1@gmail.com>
2024-01-23 15:22:43 +0100

    updated script6.js

--- script6.js
+++ script6.js
@@ -18,6 +18,35 @@ const socialObjs = [
   { name: "Joe", social: "LinkedIn" },
   { name: "Jane", social: "Instagram" },
 ];
-socialObjs.forEach((socialObj) =>
-  console.log(`${socialObj.name} - ${socialObj.social}`)
-);
+// socialObjs.forEach((socialObj) =>
+// console.log(`${socialObj.name} - ${socialObj.social}`)
+//);
+
+//filter
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const even = numbers.filter((even) => {
+  return even % 2 === 0;
+});
+// console.log(even);
+
+const odd = numbers.filter((odd) => {
+  return !(odd % 2 === 0);
+});
+// console.log(odd);
+
+//using objects
+const companies = [
+  { name: "Company-1", category: "Finance", start: 2001 },
+  { name: "Company-2", category: "Retail", start: 2002 },
+  { name: "Company-3", category: "Auto", start: 2003 },
+  { name: "Company-4", category: "Technology", start: 2004 },
+  { name: "Company-5", category: "Finance", start: 2005 },
+  { name: "Company-6", category: "Retail", start: 2006 },
+  { name: "Company-7", category: "Auto", start: 2007 },
+  { name: "Company-8", category: "Technology", start: 2008 },
+  { name: "Company-9", category: "Finance", start: 2009 },
+  { name: "Company-10", category: "Finance", start: 2010 },
+];
+//getting only retail company
+const retailCoy = companies.filter((company) => company.category === "Retail");
+console.log(retailCoy);
