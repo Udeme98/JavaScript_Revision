877b8c18d2fad5604ee0daad015b132fdeee898d
Udeme98 <Darealudeme1@gmail.com>
2024-01-24 05:13:49 +0100

    updated script6.js

--- script6.js
+++ script6.js
@@ -49,4 +49,37 @@ const companies = [
 ];
 //getting only retail company
 const retailCoy = companies.filter((company) => company.category === "Retail");
-console.log(retailCoy);
+// console.log(retailCoy);
+
+//map
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const doubleNums = nums.map((num) => {
+  return num * 2;
+});
+// console.log(doubleNums);
+
+//using map to get just the company names
+const coyNames = companies.map((coyName) => {
+  return coyName.name;
+});
+// console.log(coyNames);
+
+// array with just company and category
+const nameCats = companies.map((nameCat) => {
+  return [nameCat.name, nameCat.category];
+});
+// console.log(nameCats);
+
+//chaining map methods
+const sqrAndDoub = nums.map((num) => num ** 2).map((num) => num * 2);
+// console.log(sqrAndDoub);
+
+//chaining different array methods
+const evenN_double = numbers
+  .filter((even) => {
+    return even % 2 === 0;
+  })
+  .map((evenD) => {
+    return evenD * 2;
+  });
+console.log(evenN_double);
