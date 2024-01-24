c14efef6a98d227d97be34ebf7e0aa6db0925fef
Udeme98 <Darealudeme1@gmail.com>
2024-01-24 22:09:10 +0100

    created script7.js

---
+++ script7.js
@@ -0,0 +1,16 @@
+// Document Object Model(DOM) - What is the DOM?
+// - Programming interface for web/HTML elements
+// - Structure that we can interact with via JavaScript
+// - Includes tags, attributes, text node, etc
+// - Represented as a tree structure
+
+// console.dir(window.document);
+// console.dir(document);
+// HTMLCollection - similar to an array!
+
+//DoM methods
+// document.write("Hello Udeme");
+// console.log(document.getElementById("main"));
+const main = document.getElementById("main");
+// main.innerHTML = 'Hello';
+// document.querySelector("#main h1").innerText = "Udeme";
