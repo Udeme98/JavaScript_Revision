9aa165fa2e43b2157aa63b6554bab323fe82118e
Udeme98 <Darealudeme1@gmail.com>
2024-01-25 22:16:44 +0100

    updated shop.js

--- DOM/DOM Intro/script7.js
+++ DOM/DOM Intro/script7.js
@@ -14,3 +14,32 @@
 const main = document.getElementById("main");
 // main.innerHTML = 'Hello';
 // document.querySelector("#main h1").innerText = "Udeme";
+
+// DOM Properties
+let output;
+
+output = document.all[11];
+output = document.all.length;
+output = document.documentElement;
+
+output = document.head;
+output = document.body;
+output = document.body.children;
+
+output = document.doctype;
+output = document.domain;
+output = document.URL;
+output = document.characterSet;
+output = document.characterSet;
+
+output = document.forms;
+output = document.forms[0];
+output = document.forms[0].id;
+
+output = document.link;
+output = document.images;
+
+const forms = Array.from(document.forms);
+forms.forEach((form) => console.log(form));
+
+console.log(output);
--- shop.js
+++ shop.js
@@ -1,1 +1,5 @@
-console.log("DOM");
+// DOM Selectors - Single elements
+// Methods to more easily select elements from the DOM
+//document.getElementById()
+console.log(document.getElementById("app-title"));
+console.log(document.getElementById("app-title").id);
