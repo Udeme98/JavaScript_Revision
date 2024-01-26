7d441397c839658a79b01160a04c5a19c121876e
Udeme98 <Darealudeme1@gmail.com>
2024-01-26 15:09:56 +0100

    updated script7.js

--- DOM/DOM Intro/script7.js
+++ DOM/DOM Intro/script7.js
@@ -43,3 +43,30 @@ const forms = Array.from(document.forms);
 forms.forEach((form) => console.log(form));
 
 console.log(output);
+
+//selectors used for single elements
+// document.querySelector()
+let select;
+select = document.querySelector("h1"); //select a single element
+select = document.querySelector("#app-title"); // selects a single id
+select = document.querySelector(".container"); //selects a single class
+select = document.querySelector("input[type='text']"); //selects an attribute
+select = document.querySelector("li:nth-child(2)"); //using a pseudo selector
+
+//console.log(select);
+
+//usimg this methods on other elements
+const list = document.querySelector("ul");
+const firstItem = list.querySelector("li");
+// console.log(firstItem);
+
+//selectors used for multiple elements
+// document.querySelectorAll()
+const listItems = document.querySelectorAll(".items"); // returns a nodelist
+listItems[0].style.color = "red";
+
+console.log(listItems[0]);
+
+//Old Methods
+// - document.getElementsByClassName()
+// - document.getElementsByTagName()
