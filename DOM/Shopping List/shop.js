a5a04b6380da5308c925480a7e4aaf02f2debff2
Udeme98 <Darealudeme1@gmail.com>
2024-01-28 13:22:36 +0100

    updated shop.js

--- DOM/Shopping List/shop.js
+++ DOM/Shopping List/shop.js
@@ -24,4 +24,22 @@ const addListItem = (item) => {
           </button>`;
   document.querySelector("ul").appendChild(li);
 };
-addListItem("Mango");
+// addListItem("Mango");
+
+//new and more concise method
+const newItem = (item) => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement("button");
+  button.className = "remove-item btn-link text-red";
+
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  document.querySelector("ul").appendChild(li);
+};
+newItem("Mango");
