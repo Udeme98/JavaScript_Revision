9b026bdcb35ddc6aa3979961f39c5f63d7d23ead
Udeme98 <Darealudeme1@gmail.com>
2024-01-28 13:51:29 +0100

    updated shop.js and dom.js

--- DOM/DOM Intro/dom.js
+++ DOM/DOM Intro/dom.js
@@ -63,3 +63,31 @@ console.log(listItems[0]);
 //Old Methods
 // - document.getElementsByClassName()
 // - document.getElementsByTagName()
+
+//Create and Append Elements
+//document.createElement
+const div = document.createElement("div"); //creates an element
+div.className = "new-class"; //adds a new class
+div.id = "new-id"; //adds a new id
+div.setAttribute("title", "Shopping"); //adds an attribute
+
+//appending an innerText using appendChild()
+const text = document.createTextNode("Hello World"); //creates an innerText
+div.appendChild(text);
+// document.body.appendChild(div);
+
+// document.querySelector("ul").appendChild(div);
+
+// console.log(div);
+
+//innerHTML & CreateElement
+// not too good method
+const addListItem = (item) => {
+  const li = document.createElement("li");
+  li.innerHTML = `${item}
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+  document.querySelector("ul").appendChild(li);
+};
+// addListItem("Mango");
--- DOM/Shopping List/shop.js
+++ DOM/Shopping List/shop.js
@@ -1,41 +1,10 @@
-//Create and Append Elements
-//document.createElement
-const div = document.createElement("div"); //creates an element
-div.className = "new-class"; //adds a new class
-div.id = "new-id"; //adds a new id
-div.setAttribute("title", "Shopping"); //adds an attribute
-
-//appending an innerText using appendChild()
-const text = document.createTextNode("Hello World"); //creates an innerText
-div.appendChild(text);
-// document.body.appendChild(div);
-
-// document.querySelector("ul").appendChild(div);
-
-// console.log(div);
-
-//innerHTML & CreateElement
-// not too good method
-const addListItem = (item) => {
-  const li = document.createElement("li");
-  li.innerHTML = `${item}
-          <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-          </button>`;
-  document.querySelector("ul").appendChild(li);
-};
-// addListItem("Mango");
-
 //new and more concise method
 const newItem = (item) => {
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(item));
 
-  const button = document.createElement("button");
-  button.className = "remove-item btn-link text-red";
-
-  const icon = document.createElement("i");
-  icon.className = "fa-solid fa-xmark";
+  const icon = createIcon("fa-solid fa-xmark");
+  const button = createButton("remove-item btn-link text-red");
 
   button.appendChild(icon);
   li.appendChild(button);
@@ -43,3 +12,18 @@ const newItem = (item) => {
   document.querySelector("ul").appendChild(li);
 };
 newItem("Mango");
+
+// Refactoring to Multiple Functions
+//button function
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  return button;
+}
+
+//icon function
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
