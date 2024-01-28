514e678b7c1709952add4c3726d7926dff558183
Udeme98 <Darealudeme1@gmail.com>
2024-01-28 07:18:38 +0100

    updated shop.js

--- DOM/Shopping List/shop.js
+++ DOM/Shopping List/shop.js
@@ -1,1 +1,15 @@
-//Create and Append Element
+//Create and Append Elements
+//document.createElement
+const div = document.createElement("div"); //creates an element
+div.className = "new-class"; //adds a new class
+div.id = "new-id"; //adds a new id
+div.setAttribute("title", "Shopping"); //adds an attribute
+
+//appending an innerText using appendChild()
+const text = document.createTextNode("Hello World"); //creates an innerText
+div.appendChild(text);
+// document.body.appendChild(div);
+
+// document.querySelector("ul").appendChild(div);
+
+console.log(div);
