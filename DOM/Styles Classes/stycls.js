b55b3b352ecc055b14114c4ae2ec748e46669008
Udeme98 <Darealudeme1@gmail.com>
2024-01-31 10:04:05 +0100

    updated stycls.js

--- DOM/Styles Classes/stycls.js
+++ DOM/Styles Classes/stycls.js
@@ -4,8 +4,19 @@ const items = itemList.querySelectorAll("li");
 
 function run() {
   //className
-  console.log(itemList.className);
-  text.className = "dark card";
+  // console.log(itemList.className);
+  // text.className = "dark card";
+
+  // classList
+  console.log(itemList.classList); //gives a tokenList
+
+  text.classList.add("dark"); //adding a class
+  text.classList.remove("card"); //removing a class
+  text.classList.toggle("hidden"); //toggles a class
+  text.classList.replace("card", "dark"); //replaces a class
+
+  //change style directly
+  itemList.style.color = "red";
 }
 
 document.querySelector(".button").onclick = run;
