7f29dde0f9e70d64acda8b56f3ffc0abe5a24d76
Udeme98 <Darealudeme1@gmail.com>
2024-02-01 21:07:17 +0100

    updated event.js and shop.html

---
+++ DOM/Shopping List/event.js
@@ -0,0 +1,30 @@
+// alert("DOM elements");
+
+const clearBtn = document.querySelector("#clear");
+const lis = document.querySelectorAll("li");
+
+function clear() {
+  lis.forEach((li) => li.remove());
+}
+
+//JavaScript Event Listener
+// clearBtn.onclick = function () {
+//   alert("Clear Button");
+// };
+
+//modern way - addEventLIstener
+clearBtn.addEventListener("click", function () {
+  //   alert("Hello World");
+});
+
+//using a named function
+clearBtn.addEventListener("click", clear);
+
+//removing an event listener using setTimeout
+// setTimeout(() => clearBtn.removeEventListener("click", clear), 5000);
+
+//triggering an event listener automatically using setTimeout
+// setTimeout(() => clearBtn.click(), 5000);
+
+//clearing the list items
+// clearBtn.addEventListener("click", clear);
