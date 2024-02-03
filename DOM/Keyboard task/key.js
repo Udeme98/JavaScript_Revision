2c429342eecc3acde6a3c4f291c60409b130f7bb
Udeme98 <Darealudeme1@gmail.com>
2024-02-03 08:14:22 +0100

    updated key.js

--- DOM/Keyboard task/key.js
+++ DOM/Keyboard task/key.js
@@ -1,3 +1,4 @@
+//my solution
 const body = document.querySelector("body");
 
 function eKey(e) {
@@ -13,4 +14,45 @@ function eKey(e) {
     }
   });
 }
-body.addEventListener("keypress", eKey);
+// body.addEventListener("keypress", eKey);
+
+//solutions:
+// Method 1
+// window.addEventListener("keypress", (e) => {
+//const insert = document.getElementById("insert");
+
+//insert.innerHTML = `
+//    <div class="key">${e.key === " " ? "space" : e.key}<small>e.key</small></div>
+//       <div class="key">${e.keyCode}<small>e.keyCode</small></div>
+//       <div class="key">${e.code}<small>e.code</small></div>
+//  `;
+// });
+
+//Method 2
+function showKeyCodes(e) {
+  const insert = document.getElementById("insert");
+  insert.innerHTML = "";
+
+  const keyCodes = {
+    "e.key": e.key === " " ? "space" : e.key,
+    "e.keyCode": e.keyCode,
+    "e.code": e.code,
+  };
+
+  for (const key in keyCodes) {
+    const div = document.createElement("div");
+    div.className = "key";
+    const small = document.createElement("small");
+
+    const keyText = document.createTextNode(key);
+    const valueText = document.createTextNode(keyCodes[key]);
+
+    small.appendChild(keyText);
+    div.appendChild(valueText);
+    div.appendChild(small);
+
+    insert.appendChild(div);
+  }
+}
+
+window.addEventListener("keypress", showKeyCodes);
