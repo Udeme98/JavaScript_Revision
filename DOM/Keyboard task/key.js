1a987608df01e4557f429f6c75e5a1d554799cc3
Udeme98 <Darealudeme1@gmail.com>
2024-02-03 05:56:53 +0100

    updated key.js

--- DOM/Keyboard task/key.js
+++ DOM/Keyboard task/key.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const body = document.querySelector("body");
+
+function eKey(e) {
+  const items = document.querySelectorAll(".key");
+
+  items.forEach((tem, index) => {
+    if (index === 0) {
+      tem.textContent = e.key;
+    } else if (index === 1) {
+      tem.textContent = e.keyCode;
+    } else {
+      tem.textContent = e.code;
+    }
+  });
+}
+body.addEventListener("keypress", eKey);
