5753903ae199d5181a3bb75a806dcd7a94cbef7f
Udeme98 <Darealudeme1@gmail.com>
2024-02-22 11:27:16 +0100

    created includes.js file

---
+++ Dylan/includes.js
@@ -0,0 +1,9 @@
+// In the below array, check if the array contains chocolate
+
+const listIngredients = ["flour", "sugar", "eggs", "butter"];
+
+if (listIngredients.includes("chocolate")) {
+  console.log("We making a chocolate cake");
+} else {
+  console.log("We aren't making a chocolate cake");
+}
